e7ab6cb4f46fc8f53e23534eb25e4a114b74f848
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-04 22:43:14 -0300

    feat(CreateUserController): add account creation of the user

--- server/src/controllers/user/CreateUserController.ts
+++ server/src/controllers/user/CreateUserController.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from 'express';
+import z from 'zod';
 
 import { CreateUserService } from '../../services/user/CreateUserService.js';
 
 export class CreateUserController {
   static async handle(req: Request, res: Response) {
-    const user = await CreateUserService.execute();
+    const userData = z
+      .object({
+        name: z.string().min(3),
+        email: z.email(),
+        phone: z.string(),
+        password: z.string(),
+        confirm: z.string(),
+      })
+      .refine((data) => data.password === data.confirm, {
+        message: 'As senhas devem ser iguais.',
+        path: ['confirmpassword'],
+      })
+      .parse(req.body);
 
-    return res.json(user);
+    const user = await CreateUserService.execute(userData);
+
+    return res.json({ email: user.email, password: user.password });
   }
 }
--- server/src/services/user/CreateUserService.ts
+++ server/src/services/user/CreateUserService.ts
@@ -1,5 +1,42 @@
+import bcrypt from 'bcryptjs';
+
+import ApiError from '../../utils/ApiError.js';
+import User from '../../models/User.js';
+
+interface UserData {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+}
+
 export class CreateUserService {
-  static async execute() {
-    
+  static async execute({ name, email, phone, password }: UserData) {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      throw new ApiError('Este e-mail já está em uso!', 422);
+    }
+
+    // create a password hash
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // create a user
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    });
+
+    try {
+      const newUser = await user.save();
+
+      return newUser;
+    } catch (err) {
+      console.error(err);
+      throw new Error(err as string);
+    }
   }
 }
