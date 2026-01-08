d160cc51169f60f7b3b0782cb662a82a7ebc153d
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-07 23:13:53 -0300

    feat(schemas): stores schemas related to the pet within the schemas directory

---
+++ server/src/schemas/pet.ts
@@ -0,0 +1,47 @@
+import z from 'zod';
+
+import { MulterImageData } from '../@types/image.js';
+
+export const multerImageDataSchema = z.object({
+  fieldname: z.string(),
+  originalname: z.string(),
+  encoding: z.string(),
+  mimetype: z.string(),
+  path: z.string(),
+  size: z.number(),
+  filename: z.string(),
+}) satisfies z.ZodType<MulterImageData>;
+
+export const createPetSchema = z.object({
+  name: z.string().min(2),
+  age: z.coerce.number().min(0),
+  weight: z.coerce.number(),
+  description: z.string(),
+  breed: z.string(),
+  gender: z.string(),
+  castrationStatus: z
+    .enum(['true', 'false'], {
+      message: 'castrationStatus deve ser "true" ou "false"',
+    })
+    .transform((val) => val === 'true'),
+  location: z.string().transform((str, ctx) => {
+    try {
+      const parsed = JSON.parse(str);
+      return z
+        .object({ state: z.string(), municipality: z.string() })
+        .parse(parsed);
+    } catch (error) {
+      ctx.addIssue({
+        code: 'custom',
+        message:
+          'Location deve ser uma string JSON válida com state e municipality',
+      });
+      return z.NEVER;
+    }
+  }),
+});
+
+export const petImagesSchema = z.array(
+  multerImageDataSchema,
+  'É necessário enviar pelo menos 1 imagem do pet'
+);
