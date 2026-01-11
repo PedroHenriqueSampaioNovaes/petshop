146c01daaf94c75ad3196b89d307658a4293be38
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-11 15:59:51 -0300

    refactor(CreateUserController): import and use schema of the user

--- server/src/controllers/user/CreateUserController.ts
+++ server/src/controllers/user/CreateUserController.ts
@@ -1,23 +1,12 @@
 import { Request, Response } from 'express';
-import z from 'zod';
 
 import { CreateUserService } from '../../services/user/CreateUserService.js';
 
+import { userSchema } from '../../schemas/user.js';
+
 export class CreateUserController {
   static async handle(req: Request, res: Response) {
-    const userData = z
-      .object({
-        name: z.string().min(3),
-        email: z.email('Insira um e-mail válido.'),
-        phone: z.string(),
-        password: z.string(),
-        confirm: z.string(),
-      })
-      .refine((data) => data.password === data.confirm, {
-        message: 'As senhas devem ser iguais.',
-        path: ['confirmpassword'],
-      })
-      .parse(req.body);
+    const userData = userSchema.parse(req.body);
 
     const user = await CreateUserService.execute(userData);
 
