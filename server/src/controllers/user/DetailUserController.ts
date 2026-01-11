cf9ca9136da443a033ca19a93bfe9b0208d03346
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-11 20:31:07 -0300

    refactor(user): add debug log in DetailUserController
    Add console log to help track user data flow during debugging sessions

--- server/src/controllers/user/DetailUserController.ts
+++ server/src/controllers/user/DetailUserController.ts
@@ -4,6 +4,7 @@ import { DetailUserService } from '../../services/user/DetailUserService.js';
 
 export class DetailUserController {
   static async handle(req: Request, res: Response) {
+    console.log('user');
     const user = await DetailUserService.execute({ userId: req.user_id });
 
     return res.json(user);
