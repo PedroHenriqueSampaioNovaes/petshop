d9a3807390bdfb75ec03b6d3351804730e439c21
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-11 22:03:39 -0300

    refactor(routes): remove debug console.log statement

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, pathToFileURL } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log('dirname', __dirname, path.resolve(__dirname));
-console.log(fs.readdirSync(__dirname));
+// console.log(fs.readdirSync(__dirname));
 
 const files = fs
   .readdirSync(__dirname)
