f1a65b98bd4c23a5f1c2000a658d46f01ae2b12a
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-11 21:28:45 -0300

    refactor: remove debug console.log statements

--- server/src/controllers/user/DetailUserController.ts
+++ server/src/controllers/user/DetailUserController.ts
@@ -4,7 +4,6 @@ import { DetailUserService } from '../../services/user/DetailUserService.js';
 
 export class DetailUserController {
   static async handle(req: Request, res: Response) {
-    console.log('user');
     const user = await DetailUserService.execute({ userId: req.user_id });
 
     return res.json(user);
