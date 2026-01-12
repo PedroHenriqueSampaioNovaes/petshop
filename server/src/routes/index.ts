a74fd9776b0b74c2b3ce8425fd8dff1650575494
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-11 22:16:40 -0300

    refactor(routes): remove debug console.log statements

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,13 +4,10 @@ import { fileURLToPath, pathToFileURL } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log('dirname', __dirname, path.resolve(__dirname));
-// console.log(fs.readdirSync(__dirname));
 
 const files = fs
   .readdirSync(__dirname)
   .filter((file) => file.indexOf('.') !== 0 && !/index.[ts|js]/.test(file));
-console.log(files);
 
 export default Promise.all(
   files.map(async (file) => {
