498cc4b62cf16570db85f3e3a83a6b3157060477
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-11 21:30:13 -0300

    refactor(routes): add debug log for directory path

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, pathToFileURL } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log('dirname', __dirname);
 
 const files = fs
   .readdirSync(path.resolve(__dirname))
