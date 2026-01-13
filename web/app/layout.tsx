38c1d67f820836e6ffdbb115c0806b09f14fc337
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-13 16:04:32 -0300

    refactor(layout): wrap children in flex container for better page structure

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${poppins.variable} antialiased font-primary`}
       >
-        {children}
+        <div className="flex flex-col min-h-screen">
+          <main className="flex-1">{children}</main>
+        </div>
       </body>
     </html>
   );
