7ca1c4392cb1e43d7717339e4dabab63bd3273a7
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-13 16:00:52 -0300

    feat(web): initialize Next.js project with basic configuration

---
+++ web/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+import { Inter, Poppins } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({
+  variable: '--font-inter-sans',
+  subsets: ['latin'],
+});
+
+const poppins = Poppins({
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: '500',
+});
+
+export const metadata: Metadata = {
+  title: 'Adopt a Pet',
+  description:
+    'Adote um pet e ajude a encontrar um lar para eles, onde poderão compartilhar muitos momentos felizes.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${inter.variable} ${poppins.variable} antialiased font-primary`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
