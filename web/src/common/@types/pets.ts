ba5c5ad7d46dd23ba6ed1eef633ebeed92b4424a
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-16 01:23:50 -0300

    feat(@types): create interfaces pet

---
+++ web/src/common/@types/pets.ts
@@ -0,0 +1,40 @@
+export interface IPet {
+  _id: string;
+  name: string;
+  age: number;
+  weight: number;
+  gender: string;
+  breed: string;
+  castrationStatus: boolean;
+  location: {
+    state: string;
+    municipality: string;
+  };
+  description: string;
+  images: {
+    url: string;
+    public_id: string;
+  }[];
+  available: boolean;
+  user: {
+    _id: string;
+    name: string;
+    image: string;
+    phone: string;
+  };
+  adopter: {
+    _id: string;
+    name: string;
+    image: {
+      url: string;
+      public_id: string;
+    };
+  };
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IApiGetPets {
+  petsPerPage?: number;
+  nextCursor?: string | null;
+}
