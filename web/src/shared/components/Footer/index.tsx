026e885d6c449ae028b1d3860a4c34aa4a17f79f
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-18 19:53:58 -0300

    refactor(footer): replace className bg-back-300 to bg-back-200

--- web/src/shared/components/Footer/index.tsx
+++ web/src/shared/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-back-300 h-20 md:h-36 flex items-center justify-center text-back-700 rounded-t-lg border-t border-back-600 font-secondary">
+    <footer className="bg-back-200 h-20 md:h-36 flex items-center justify-center text-back-700 rounded-t-lg border-t border-back-600 font-secondary">
       <p>
         <span className="text-secondary font-semibold">Adopt A Pet</span> &copy;
         2026
