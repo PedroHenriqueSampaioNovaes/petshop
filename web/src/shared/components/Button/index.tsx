bb40a11c85d832dc280cc8dc19e05bc1237abc69
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-19 15:43:56 -0300

    feat(button): create button component

---
+++ web/src/shared/components/Button/index.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { ComponentProps, ReactNode } from 'react';
+
+interface ButtonProps extends ComponentProps<'button'> {
+  className?: string;
+  children: ReactNode;
+}
+
+export default function Button({ className, children, ...props }: ButtonProps) {
+  return (
+    <button
+      className={`block cursor-pointer bg-valid-500 hover:bg-valid-600 transition text-white font-bold h-12 w-full mt-4 rounded-lg ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
