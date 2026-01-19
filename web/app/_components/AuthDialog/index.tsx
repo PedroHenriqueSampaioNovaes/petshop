563de1e74d5bca11794f5a47f57e7ae2625396a3
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-19 16:26:49 -0300

    feat(authdialog): blur effect

--- web/app/_components/AuthDialog/index.tsx
+++ web/app/_components/AuthDialog/index.tsx
@@ -34,7 +34,7 @@ export default function AuthDialog({
         {triggerText}
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Backdrop className="fixed inset-0 min-h-dvh bg-black opacity-48 transition-all duration-150 data-ending-style:opacity-0 data-starting-style:opacity-0 supports-[-webkit-touch-callout:none]:absolute" />
+        <Dialog.Backdrop className="fixed inset-0 min-h-dvh bg-black/48 backdrop-blur-xs transition-all duration-150 data-ending-style:opacity-0 data-starting-style:opacity-0 supports-[-webkit-touch-callout:none]:absolute" />
         <Dialog.Popup className="fixed top-1/2 left-1/2 -mt-8 w-[80vw] max-w-79 lg:max-w-158 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-gray-50 p-6 text-gray-900 outline-1 outline-back-300 transition-all duration-150 data-ending-style:scale-90 data-ending-style:opacity-0 data-starting-style:scale-90 data-starting-style:opacity-0">
           <div className={`flex ${reverseColumn ? 'flex-row-reverse' : ''}`}>
             <form onSubmit={handleSubmit} className="flex-1">
