3590fc5b55a0e9f92bda9508317a65c034a7e043
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-22 20:27:21 -0300

    fix(usegetpetsoninfinitescroll): now there will no longer be duplication of data returned from the fetch in the development environment

--- web/src/hooks/useGetPetsOnInfiniteScroll.tsx
+++ web/src/hooks/useGetPetsOnInfiniteScroll.tsx
@@ -1,50 +1,48 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
-import petsGet from '@/app/actions/pets-get';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import { IPet } from '@/src/common/@types/pets';
 
-interface IPetsResponse {
-  pets: IPet[];
-  hasNextPage: boolean;
-  nextCursor: string | null;
-}
+import petsGet from '@/app/actions/pets-get';
 
 export default function useGetPetsOnInfiniteScroll() {
-  const [responsePetData, setResponsePetData] = useState<IPetsResponse>({
-    pets: [],
-    nextCursor: null,
-    hasNextPage: true,
-  });
-  const [fetchPets, setFetchPets] = useState(true);
-  const pets = responsePetData.pets;
+  const [pets, setPets] = useState<IPet[]>([]);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  const nextCursorRef = useRef<string | null>(null);
+  const isFetchingRef = useRef(false);
+
+  const getPets = useCallback(async () => {
+    if (isFetchingRef.current) return;
+
+    isFetchingRef.current = true;
+
+    const { data, ok } = await petsGet({
+      nextCursor: nextCursorRef.current,
+    });
+
+    if (!ok) {
+      console.error('Falhou a busca por novos pets');
+      return;
+    }
+
+    setPets((prev) => [...prev, ...data.pets]);
+    setHasNextPage(data.hasNextPage);
+
+    nextCursorRef.current = data.nextCursor;
+    isFetchingRef.current = false;
+  }, []);
 
   useEffect(() => {
-    async function getPets() {
-      if (!fetchPets || !responsePetData.hasNextPage) return;
-
-      const { data, ok } = await petsGet({
-        nextCursor: responsePetData.nextCursor,
-      });
-
-      if (!ok) return;
-      setResponsePetData((prev) => ({
-        pets: [...prev.pets, ...data.pets],
-        nextCursor: data.nextCursor,
-        hasNextPage: data.hasNextPage,
-      }));
-      setFetchPets(false);
-    }
     getPets();
-  }, [fetchPets]);
+  }, [getPets]);
 
   useEffect(() => {
     let wait = false;
 
     function handleScroll() {
-      if (wait || !responsePetData.hasNextPage) return;
+      if (wait || !hasNextPage) return;
 
       const { scrollHeight, scrollTop } = document.documentElement;
       const viewportHeight = window.innerHeight;
@@ -53,20 +51,20 @@ export default function useGetPetsOnInfiniteScroll() {
       const canAction = finalScrollDistance < scrollTop;
 
       if (canAction) {
-        setFetchPets(true);
+        getPets();
         wait = true;
         setTimeout(() => {
           wait = false;
         }, 500);
       }
     }
+
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  }, [getPets, hasNextPage]);
 
   return {
     pets,
-    hasNextPage: responsePetData.hasNextPage,
-    nextCursor: responsePetData.nextCursor,
+    hasNextPage,
   };
 }
