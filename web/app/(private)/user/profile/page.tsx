be2ce3d3d59097e9489af7c7be8a52f639553c83
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-23 22:47:06 -0300

    feat(profile): add Metadata for SEO

--- web/app/(private)/user/profile/page.tsx
+++ web/app/(private)/user/profile/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from 'next';
+
 import Profile from './_components/Profile';
+
+export const metadata: Metadata = {
+  title: 'Adopt a Pet - Perfil',
+  description:
+    'Altere suas informações de perfil. Altere foto, nome, e-mail e etc...',
+};
 
 export default function ProfilePage() {
   return <Profile />;
