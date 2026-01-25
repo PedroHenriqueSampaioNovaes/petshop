1cc8c50f181626f3f2c3fb0fd83537d7a909d9d6
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-25 17:37:42 -0300

    feat(applayout): create app layout to organize structure and using suspense api in the layout.tsx

---
+++ web/app/_components/AppLayout/index.tsx
@@ -0,0 +1,27 @@
+import { UserContextProvider } from '@/src/context/UserContext';
+import { AuthModalContextProvider } from '@/src/context/AuthModalContext';
+
+import userGet from '@/app/actions/user-get';
+
+import Header from '@/src/shared/components/Header';
+import Footer from '@/src/shared/components/Footer';
+
+interface AppLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function AppLayout({ children }: AppLayoutProps) {
+  const { data: user } = await userGet();
+
+  return (
+    <UserContextProvider userData={user}>
+      <AuthModalContextProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-1 container">{children}</main>
+          <Footer />
+        </div>
+      </AuthModalContextProvider>
+    </UserContextProvider>
+  );
+}
--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Inter, Poppins } from 'next/font/google';
 import './globals.css';
 
-import Header from '@/src/shared/components/Header';
-import Footer from '@/src/shared/components/Footer';
-
-import userGet from './actions/user-get';
-
-import { UserContextProvider } from '@/src/context/UserContext';
-import { AuthModalContextProvider } from '@/src/context/AuthModalContext';
+import AppLayout from './_components/AppLayout';
 
 const inter = Inter({
   variable: '--font-inter-sans',
@@ -28,27 +23,19 @@ export const metadata: Metadata = {
     'Adote um pet e ajude a encontrar um lar para eles, onde poderão compartilhar muitos momentos felizes.',
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { data: user } = await userGet();
-
   return (
     <html lang="pt-br">
       <body
         className={`${inter.variable} ${poppins.variable} antialiased font-primary`}
       >
-        <UserContextProvider userData={user}>
-          <AuthModalContextProvider>
-            <div className="flex flex-col min-h-screen">
-              <Header />
-              <main className="flex-1 container">{children}</main>
-              <Footer />
-            </div>
-          </AuthModalContextProvider>
-        </UserContextProvider>
+        <Suspense>
+          <AppLayout>{children}</AppLayout>
+        </Suspense>
 
         <Toaster />
       </body>
