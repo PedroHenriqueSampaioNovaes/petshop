7a54d7c83355de24c100332d998958462e0962c6
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-25 22:50:16 -0300

    feat(subtitle): add className to props

--- web/src/shared/components/Subtitle/index.tsx
+++ web/src/shared/components/Subtitle/index.tsx
@@ -2,8 +2,13 @@ import { ReactNode } from 'react';
 
 interface SubtitleProps {
   children: ReactNode;
+  className: string;
 }
 
-export default function Subtitle({ children }: SubtitleProps) {
-  return <p className="font-secondary max-md:text-center">{children}</p>;
+export default function Subtitle({ children, className }: SubtitleProps) {
+  return (
+    <p className={`font-secondary max-md:text-center ${className}`}>
+      {children}
+    </p>
+  );
 }
