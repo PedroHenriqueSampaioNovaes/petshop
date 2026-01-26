2e8d68016c895583189221c51d8799f70bd46a38
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-26 01:01:17 -0300

    fix(subtitle): make classname optional

--- web/src/shared/components/Subtitle/index.tsx
+++ web/src/shared/components/Subtitle/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 interface SubtitleProps {
   children: ReactNode;
-  className: string;
+  className?: string;
 }
 
 export default function Subtitle({ children, className }: SubtitleProps) {
