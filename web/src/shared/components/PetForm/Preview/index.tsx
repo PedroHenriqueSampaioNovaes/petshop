f3c3febdca5ab01f1e94c950ae2e0fab118d5d07
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-29 23:04:05 -0300

    refactor(preview): refactors the algorithm, in addition to correcting the path of the PetFormSchema import
    Remove useEffect to reduce the number of re-renders

--- web/src/shared/components/PetForm/Preview/index.tsx
+++ web/src/shared/components/PetForm/Preview/index.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useFormContext } from 'react-hook-form';
 
-import { PetFormSchema } from '..';
+import { PetFormSchema } from '@/src/schema/pet';
 
 interface PreviewProps {
   petUrlImages?: string[];
@@ -12,38 +11,28 @@ interface PreviewProps {
 
 export default function Preview({ petUrlImages }: PreviewProps) {
   const { watch } = useFormContext<PetFormSchema>();
-  const [preview, setPreview] = useState<string[] | null>(petUrlImages || null);
+  const files = watch('images');
 
-  const images = watch('images');
+  let preview: string[] = petUrlImages || [];
+  if (files?.length) {
+    preview = [];
+    const filesArray = Array.from(files);
+    filesArray.forEach((file) => preview.push(URL.createObjectURL(file)));
+  }
 
-  useEffect(() => {
-    function handleFile() {
-      const files = images;
-
-      if (files) {
-        const filesArray = Array.from(files);
-        const urlsImage: string[] = [];
-        filesArray.forEach((file) => urlsImage.push(URL.createObjectURL(file)));
-        setPreview(urlsImage);
-      }
-    }
-    handleFile();
-  }, [images, setPreview]);
-
-  if (images?.length !== 4) return null;
+  if (preview?.length !== 4) return null;
   return (
     <div className="flex justify-center flex-wrap">
-      {preview &&
-        preview.map((urlImage) => (
-          <Image
-            key={urlImage}
-            src={urlImage}
-            alt="Foto do pet"
-            width={200}
-            height={200}
-            className="mx-3 mb-4 w-24 md:w-35 h-24 md:h-35 object-cover"
-          />
-        ))}
+      {preview.map((urlImage) => (
+        <Image
+          key={urlImage}
+          src={urlImage}
+          alt="Foto do pet"
+          width={200}
+          height={200}
+          className="mx-3 mb-4 w-24 md:w-35 h-24 md:h-35 object-cover"
+        />
+      ))}
     </div>
   );
 }
