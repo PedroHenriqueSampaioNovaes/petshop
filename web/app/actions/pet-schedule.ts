2dfbacbfe385ec52708c684565aa15a44db4a288
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-30 23:14:54 -0300

    feat(actions): executes updateTag after scheduling an interview with the pet's guardian

--- web/app/actions/pet-schedule.ts
+++ web/app/actions/pet-schedule.ts
@@ -1,9 +1,11 @@
 'use server';
 
 import { cookies } from 'next/headers';
+import { updateTag } from 'next/cache';
 
 import apiError from '@/src/common/utils/apiError';
 import FetchApi from '@/src/common/utils/FetchApi';
+import { cacheTagByUserId } from '@/src/common/utils/cacheTagByUserId';
 
 import { PET_SCHEDULE } from '@/src/common/api';
 
@@ -13,12 +15,16 @@ interface IPetScheduleResponse {
 
 export default async function petSchedule({ id }: { id: string }) {
   try {
+    const tag = await cacheTagByUserId('get-adoptions');
+
     const token = (await cookies()).get('token')?.value;
 
     const { url } = PET_SCHEDULE({ id });
     const data = await FetchApi.patch<IPetScheduleResponse>(url, {
       token,
     });
+
+    updateTag(tag);
 
     return { data, ok: true, error: '' };
   } catch (error) {
