c89633ad73d67998fd6211c1e8585bc0f6d9d3b4
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-01-31 01:55:26 -0300

    fix(proxy): remove public of the matcher config

--- web/proxy.ts
+++ web/proxy.ts
@@ -72,6 +72,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|public).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 };
