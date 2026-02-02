2c078eb617cd0102ef5b04327bad6da6bc9facfc
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-02-02 15:04:25 -0300

    feat(db): now, when it fails to connect to the database, a connection attempt is made 5 times

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -1,18 +1,22 @@
 import mongoose from 'mongoose';
 
-async function main() {
+async function connectDB(retries = 5) {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@petshop.53mfura.mongodb.net/${process.env.DB}`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@petshop.53mfura.mongodb.net/${process.env.DB}`,
     );
     console.log('Conectou-se ao MongoDB');
   } catch (error) {
-    console.error(
-      'Um erro ocorreu ao tentar se conectar ao banco de dados:',
-      error
-    );
+    console.log(error)
+    if (retries === 0)
+      console.error(
+        'Um erro ocorreu ao tentar se conectar ao banco de dados:',
+        error,
+      );
+    await new Promise((r) => setTimeout(r, 1000));
+    connectDB(retries - 1);
   }
 }
-main();
+connectDB();
 
 export default mongoose;
