5d9261e92342dfca99055e69a9e4d08e4970cdbb
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-04-25 14:09:41 -0300

    change string connection to mongodb

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 async function connectDB(retries = 5) {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@petshop.53mfura.mongodb.net/${process.env.DB}`,
+      `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ac-oaoeeww-shard-00-00.53mfura.mongodb.net:27017,ac-oaoeeww-shard-00-01.53mfura.mongodb.net:27017,ac-oaoeeww-shard-00-02.53mfura.mongodb.net:27017/${process.env.DB}?ssl=true&replicaSet=atlas-4s5s8v-shard-0&authSource=admin&retryWrites=true&w=majority&appName=petshop`,
     );
     console.log('Conectou-se ao MongoDB');
   } catch (error) {
-    console.log(error)
+    console.log(error);
     if (retries === 0)
       console.error(
         'Um erro ocorreu ao tentar se conectar ao banco de dados:',
