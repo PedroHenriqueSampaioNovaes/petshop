0543523922b04e962303b912809dc05705c9c027
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-04-27 22:02:19 -0300

    fix: QueryClient is now correctly called within a useState

--- web/src/lib/tanstackQueryProvider.tsx
+++ web/src/lib/tanstackQueryProvider.tsx
@@ -2,15 +2,17 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import { useState } from 'react';
+
 interface TanStackQueryProviderProps {
   children: React.ReactNode;
 }
 
-const queryClient = new QueryClient();
-
 export default function TanStackQueryProvider({
   children,
 }: TanStackQueryProviderProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
